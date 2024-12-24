fd04018cf86a51ee60719635718f4efafd2d8c46
andrielson <andrielson@gmail.com>
2024-12-24 20:03:57 +0000

    test gemini thinking model

--- aider-gemini-architect.sh
+++ aider-gemini-architect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aider --architect --model gemini/gemini-2.0-flash-exp --editor-model gemini/gemini-2.0-flash-exp --editor-edit-format editor-diff --yes-always;
+aider --architect --model gemini/gemini-2.0-flash-thinking-exp-1219 --editor-model gemini/gemini-2.0-flash-exp --editor-edit-format editor-diff --yes-always;
